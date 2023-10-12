477600a9606cbeefebf2748cf2627b8c83d0778b
Dave Welsh <dave@qccareerschool.com>
2023-10-12 17:04:56 -0400

    Only preload metadata

--- src/components/nathanVideo/index.tsx
+++ src/components/nathanVideo/index.tsx
@@ -5,7 +5,7 @@ import PosterImage from './poster-with-play-button.jpg';
 const src = 'https://a853d550d96942a102ed-03698ba952ff3c4776ee22874ecfcda2.ssl.cf1.rackcdn.com/download-the-course-catalog.mp4';
 
 export const NathanVideo: FC = () => (
-  <video poster={PosterImage.src} preload="auto" controls style={{ width: '100%' }}>
+  <video poster={PosterImage.src} preload="metadata" controls style={{ width: '100%' }}>
     <source src={src} type="video/mp4" />
   </video>
 );
