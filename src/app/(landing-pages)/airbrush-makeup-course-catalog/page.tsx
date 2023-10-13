86e7fc03d5b5824e71dbce206da29766374e432c
Dave Welsh <dave@qccareerschool.com>
2023-10-13 14:27:04 -0400

    Added test AB page

---
+++ src/app/(landing-pages)/airbrush-makeup-course-catalog/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+import type { FC } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Get Started with a Free Course Catalog - QC Makeup Academy',
+  description: '....................',
+};
+
+const AirbrushMakeupPage: FC = () => {
+  return null;
+};
+
+export default AirbrushMakeupPage;
