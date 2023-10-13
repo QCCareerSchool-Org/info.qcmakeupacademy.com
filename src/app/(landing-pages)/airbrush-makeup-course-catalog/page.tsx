e0663e3cd30211eefd2e85fa8a711a28662c1b7d
Dave Welsh <dave@qccareerschool.com>
2023-10-13 15:29:13 -0400

    Updated title and description

--- src/app/(landing-pages)/airbrush-makeup-course-catalog/page.tsx
+++ src/app/(landing-pages)/airbrush-makeup-course-catalog/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next';
 import type { FC } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Get Started with a Free Course Catalog - QC Makeup Academy',
-  description: '....................',
+  title: 'Get Started with a Free Airbrush Makeup Course Catalog - QC Makeup Academy',
+  description: 'Start your airbrush makeup career with our flexible online classes. Download our course catalog to learn more about QC makeup school.',
 };
 
 const AirbrushMakeupPage: FC = () => {
