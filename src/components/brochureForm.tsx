c7b90a945cb1e02df1988883d1d8f247c7e1901c
Dave Welsh <dave@qccareerschool.com>
2023-10-13 16:36:53 -0400

    Linked form

--- src/components/brochureForm.tsx
+++ src/components/brochureForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useId, useState } from 'react';
+import { useCallback, useId, useRef, useState } from 'react';
 import type { FC, FormEventHandler } from 'react';
 import { GoogleReCaptcha } from 'react-google-recaptcha-v3';
 
@@ -11,6 +11,10 @@ type Props = {
 
 export const BrochureForm: FC<Props> = ({ action, buttonText = 'Get the Catalog' }) => {
   const id = useId();
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailAddressRef = useRef<HTMLInputElement>(null);
+  const emailOptInRef = useRef<HTMLInputElement>(null);
   const [ token, setToken ] = useState<string>();
   const [ refreshReCaptcha, setRefreshReCaptcha ] = useState(false);
 
@@ -18,25 +22,49 @@ export const BrochureForm: FC<Props> = ({ action, buttonText = 'Get the Catalog'
     setToken(t);
   }, []);
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = () => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
     console.log(token);
-    setRefreshReCaptcha(r => !r);
+    const form = e.target as HTMLFormElement;
+
+    const body = {
+      'firstName': firstNameRef.current?.value,
+      'lastName': lastNameRef.current?.value,
+      'emailAddress': emailAddressRef.current?.value,
+      'g-recaptcha-response': token,
+      'testGroup': 1,
+      'school': 'QC Makeup Academy',
+    };
+
+    fetch(form.action, {
+      method: form.method,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    }).then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+    }).catch(err => {
+      console.error(err);
+    }).finally(() => {
+      setRefreshReCaptcha(r => !r);
+    });
   };
 
   return (
     <form onSubmit={handleSubmit} action={action} method="post">
       <div className="mb-3">
         <label htmlFor={`${id}firstName`} className="form-label">Name</label>
-        <input type="text" name="firstName" id={`${id}firstName`} className="form-control" autoComplete="given-name" />
+        <input ref={firstNameRef} type="text" name="firstName" id={`${id}firstName`} className="form-control" autoComplete="given-name" />
       </div>
+      <input ref={lastNameRef} type="hidden" name="lastName" />
       <div className="mb-3">
         <label htmlFor={`${id}emailAddress`} className="form-label">Email</label>
-        <input type="email" name="emailAddress" id={`${id}emailAddress`} className="form-control" autoComplete="email" />
+        <input ref={emailAddressRef} type="email" name="emailAddress" id={`${id}emailAddress`} className="form-control" autoComplete="email" />
       </div>
-      <input type="hidden" name="lastName" />
       <div className="mb-3">
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" value="" id={`${id}emailOptIn`} />
+          <input ref={emailOptInRef} className="form-check-input" type="checkbox" value="" id={`${id}emailOptIn`} />
           <label className="form-check-label" htmlFor={`${id}emailOptIn`}>
             I agree to receive additional emails from QC, including promotions, course launches, special offers and more. Unsubscribe anytime!
           </label>
