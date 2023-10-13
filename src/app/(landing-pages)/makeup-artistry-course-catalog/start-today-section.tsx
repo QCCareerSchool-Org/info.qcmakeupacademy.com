6e380178627168667470f9c16e1e495e1a826ec8
Dave Welsh <dave@qccareerschool.com>
2023-10-13 14:08:38 -0400

    Added catalog link

--- src/app/(landing-pages)/makeup-artistry-course-catalog/start-today-section.tsx
+++ src/app/(landing-pages)/makeup-artistry-course-catalog/start-today-section.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FC } from 'react';
 
 export const StartTodaySection: FC = () => (
@@ -7,7 +8,7 @@ export const StartTodaySection: FC = () => (
         <div className="col-12 col-sm-10 col-md-8 col-lg-6">
           <h2 className="h1">Start Your Career Today!</h2>
           <p>Request a free course catalog today to learn more about how you can become a professional makeup artist with QC's online training! The catalog includes course curriculum, tuition information, and your career outlook.</p>
-          <button className="btn btn-primary btn-lg">Download Catalog</button>
+          <Link href="#"><button className="btn btn-primary btn-lg">Download Catalog</button></Link>
         </div>
       </div>
     </div>
