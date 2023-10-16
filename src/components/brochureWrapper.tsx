81603b6036f5ea1e08efd2fe01b089e1487a6247
Dave Welsh <dave@qccareerschool.com>
2023-10-16 10:10:14 -0400

    Created white variant

--- src/components/brochureWrapper.tsx
+++ src/components/brochureWrapper.tsx
@@ -2,8 +2,12 @@ import type { FC, PropsWithChildren } from 'react';
 
 import styles from './brochureWrapper.module.scss';
 
-export const BrochureWrapper: FC<PropsWithChildren> = ({ children }) => (
-  <div className={`card text-white ${styles.wrapper}`}>
+type Props = {
+  variant?: 'black' | 'white';
+};
+
+export const BrochureWrapper: FC<PropsWithChildren<Props>> = ({ variant = 'black', children }) => (
+  <div className={`card ${styles.wrapper} ${variant === 'white' ? styles.white : styles.black}`}>
     <div className="card-body p-4">
       {children}
     </div>
