89fe3f1a9157683610a6117ab2d26ef9e421150f
Dave Welsh <dave@qccareerschool.com>
2023-10-17 15:14:12 -0400

    Removed google font preconnect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,8 +41,6 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         {process.env.VWO_ID && <VWO id={parseInt(process.env.VWO_ID, 10)} />}
       </head>
       <body>
