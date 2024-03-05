869c9e574d45a677fc843e8360f891e6c10bbc88
Dave Welsh <dave@qccareerschool.com>
2024-03-05 15:59:10 -0500

    Updated makeup kit

--- src/app/(landing-pages)/makeup-artistry-course-catalog/makeup-kit-section.tsx
+++ src/app/(landing-pages)/makeup-artistry-course-catalog/makeup-kit-section.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { FC } from 'react';
 
-import MakeupKitImage from './makeup-kit-image.jpg';
+import MakeupKitImage from './makeup-kit-image-no-lashes.jpg';
 
 export const MakeupKitSection: FC = () => (
   <section className="bg-black text-white">
@@ -9,7 +9,7 @@ export const MakeupKitSection: FC = () => (
       <div className="row justify-content-center">
         <div className="col-12 col-lg-8 col-xxl-6">
           <h2 className="h1">Bonus Luminous Makeup Kit</h2>
-          <p>When you enroll in the Master Makeup Artistry Course you'll receive a gorgeous 10-piece makeup kit to help you launch your professional career!</p>
+          <p>When you enroll in the Master Makeup Artistry Course you'll receive a gorgeous 8-piece makeup kit to help you launch your professional career!</p>
           <p className="small fst-italic">Makeup kits available while supplies last. You will receive your items once you have completed Unit A.</p>
         </div>
       </div>
