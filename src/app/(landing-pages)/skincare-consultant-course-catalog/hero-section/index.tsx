8ab68e0a3e70bf8c3f3c035bc7ac5acb484938bf
Dave Welsh <dave@qccareerschool.com>
2024-06-17 13:32:43 -0400

    Added form action url

--- src/app/(landing-pages)/skincare-consultant-course-catalog/hero-section/index.tsx
+++ src/app/(landing-pages)/skincare-consultant-course-catalog/hero-section/index.tsx
@@ -9,6 +9,8 @@ type Props = {
   alt?: boolean;
 };
 
+const action = 'http://go.qcmakeupacademy.com/l/947642/2024-06-17/x31bx';
+
 export const HeroSection: FC<Props> = ({ alt }) => (
   <section className="text-white">
     <BackgroundImage desktopSrc={HeroImage} desktopObjectPosition="50% 0%" mobileSrc={HeroImage} mobileObjectPosition="100% 50%" priority />
@@ -17,7 +19,7 @@ export const HeroSection: FC<Props> = ({ alt }) => (
         <div className="col-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mb-4 mb-lg-0">
           <FormWrapper alt={alt}>
             <h2 className="h4">Get Your Free Skincare Course Catalog</h2>
-            <BrochureForm action="" />
+            <BrochureForm action={action} />
           </FormWrapper>
         </div>
         <div className="col-12 col-lg-6 col-xxl-5 text-center text-lg-start">
