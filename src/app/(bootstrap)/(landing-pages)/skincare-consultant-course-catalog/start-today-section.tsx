044711d2823068b5d5ac436b0d2f41af4e775f13
Dave Welsh <dave@qccareerschool.com>
2026-01-05 11:56:24 -0500

    Reduced duration

--- src/app/(bootstrap)/(landing-pages)/skincare-consultant-course-catalog/start-today-section.tsx
+++ src/app/(bootstrap)/(landing-pages)/skincare-consultant-course-catalog/start-today-section.tsx
@@ -10,7 +10,7 @@ export const StartTodaySection: FC = () => (
         <div className="col-12 col-md-10 col-lg-8 col-xl-6">
           <h2 className="h1">Start Your Career Today!</h2>
           <p>Request a free course catalog today to learn more about how you can become a professional skincare consultant with QC's online training! The catalog includes course curriculum, tuition information, and your career outlook.</p>
-          <ScrollLink smooth to="top"><button className="btn btn-primary btn-lg">Download Catalog</button></ScrollLink>
+          <ScrollLink smooth duration={300} to="top"><button className="btn btn-primary btn-lg">Download Catalog</button></ScrollLink>
         </div>
       </div>
     </div>
