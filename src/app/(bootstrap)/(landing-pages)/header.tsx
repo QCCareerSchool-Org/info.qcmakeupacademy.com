ff6390ffa042603e8535b729857491108d7facf0
Dave Welsh <dave@qccareerschool.com>
2026-01-05 11:31:10 -0500

    Fixed links

--- src/app/(bootstrap)/(landing-pages)/header.tsx
+++ src/app/(bootstrap)/(landing-pages)/header.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Link from 'next/link';
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
+import { Element as ScrollElement, Link as ScrollLink } from 'react-scroll';
 
 import styles from './header.module.scss';
 import { Logo } from '@/components/logo';
@@ -27,20 +28,30 @@ const getVisible = (screenWidth: number, scrollPosition: number): boolean => {
   return scrollPosition >= 800;
 };
 
-export const Header: FC = () => {
+interface Props {
+  buttonHref?: string;
+  buttonContent?: ReactNode | string;
+}
+
+export const Header: FC<Props> = ({ buttonHref, buttonContent = 'Get the Catalog' }) => {
   const screenWidth = useScreenWidth();
   const scrollPosition = useScrollPosition();
 
   const visible = getVisible(screenWidth, scrollPosition);
 
   return (
-    <header className={`${styles.header} ${visible ? styles.headerVisible : ''}`}>
-      <div className="container">
-        <div className="d-flex justify-content-between">
-          {screenWidth >= 400 && <Logo height={18} />}
-          <Link href="#"><button className="btn btn-primary btn-sm py-2 rounded-0">Get the Catalog</button></Link>
+    <ScrollElement name="top">
+      <header className={`${styles.header} ${visible ? styles.headerVisible : ''}`}>
+        <div className="container">
+          <div className="d-flex justify-content-between">
+            {screenWidth >= 400 && <Logo height={18} />}
+            {buttonHref
+              ? <Link href="#"><button className="btn btn-primary btn-sm py-2 rounded-0">{buttonContent}</button></Link>
+              : <ScrollLink smooth to="top"><button className="btn btn-primary btn-sm py-2 rounded-0">{buttonContent}</button></ScrollLink>
+            }
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </ScrollElement>
   );
 };
--- src/app/(bootstrap)/(landing-pages)/skincare-consultant-course-catalog/start-today-section.tsx
+++ src/app/(bootstrap)/(landing-pages)/skincare-consultant-course-catalog/start-today-section.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link';
+'use client';
+
 import type { FC } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
 
 export const StartTodaySection: FC = () => (
   <section className="bg-light">
@@ -8,7 +10,7 @@ export const StartTodaySection: FC = () => (
         <div className="col-12 col-md-10 col-lg-8 col-xl-6">
           <h2 className="h1">Start Your Career Today!</h2>
           <p>Request a free course catalog today to learn more about how you can become a professional skincare consultant with QC's online training! The catalog includes course curriculum, tuition information, and your career outlook.</p>
-          <Link href="#"><button className="btn btn-primary btn-lg">Download Catalog</button></Link>
+          <ScrollLink smooth to="top"><button className="btn btn-primary btn-lg">Download Catalog</button></ScrollLink>
         </div>
       </div>
     </div>
